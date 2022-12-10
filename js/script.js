6a339b809f0559a9926e453cc359c6907ce97582
Mariano Godoy <marianogodoyg@gmail.com>
2022-12-10 19:21:31 -0300

    cambios barra de nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelector(".nav-link");
 
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navLink.classList.toggle("active");
+})
+
+navLink.addEventListener("click", () => {
+    navLink.classList.toggle("active");
 })
