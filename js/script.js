9cd8cb51ba0aa25bee621fd43620fe00d93b232e
Mariano Godoy <marianogodoyg@gmail.com>
2022-12-11 12:10:29 -0300

    barra de nav js, css y form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelector(".nav-link");
+let menuLink1 = document.querySelector(".link1");
+let menuLink2 = document.querySelector(".link2");
+let menuLink3 = document.querySelector(".link3");
+let menuLink4 = document.querySelector(".link4");
 
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
-    navLink.classList.toggle("active");
-})
-
-navLink.addEventListener("click", () => {
-    navLink.classList.toggle("active");
-})
+});
+menuLink1.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+menuLink2.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+menuLink3.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+menuLink4.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
