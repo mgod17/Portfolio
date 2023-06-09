764cb73a877dae7f48eb427e9f6854f917757bb9
mgod17 <marianogodoyg@gmail.com>
2023-06-09 19:51:38 -0300

    optimizacion de js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
-// MENU
 const burgerMenu = document.querySelector(".burger-menu");
 const navMenu = document.querySelector(".nav-menu");
-let menuLink1 = document.querySelector(".link1");
-let menuLink2 = document.querySelector(".link2");
-let menuLink3 = document.querySelector(".link3");
-let menuLink4 = document.querySelector(".link4");
+const menuLinks = document.querySelectorAll(".nav-link");
 
-burgerMenu.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
-menuLink1.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
-menuLink2.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
-menuLink3.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
-menuLink4.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
+function toggleMenu() {
+  burgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  burgerMenu.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+burgerMenu.addEventListener("click", toggleMenu);
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
 });
